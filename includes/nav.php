60fa0299faf6ee672b97c3caebb526559607a98d
Nico_le <156796855+NicoleMaini@users.noreply.github.com>
2024-05-10 17:04:16 +0200

    change language

--- includes/nav.php
+++ includes/nav.php
@@ -1,13 +1,32 @@
+<?php
+include_once __DIR__ . '/constants.php';
+include_once __DIR__ . '/translations.php';
+
+
+// determinare la lingua scelta nel cookie
+$cookie_expiration = time() + 60 * 60 * 24 * 365 * 10;
+
+if (!isset($_COOKIE['language'])) {
+    setcookie('language', 'it', $cookie_expiration);
+    $language = 'it';
+    echo 'sono qui dentro';
+} else {
+    $language = $_COOKIE['language'];
+    echo $language;
+}
+
+?>
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <form action="" method="get">
-            <select class="form-select" aria-label="Default select example">
-                <option selected>Open this select menu</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+        <a class="navbar-brand" href="<?= SITE_URL ?>"><?= $labels[$language]['site_name'] ?></a>
+        <form action="<?= SITE_URL . '/change-language.php' ?>" method="get" class="d-flex">
+            <select name="language" class="form-select" aria-label="Default select example">
+                <option value="it" <?= $language === 'it' ? ' selected' : '' ?>>Italiano</option>
+                <option value="en" <?= $language === 'en' ? ' selected' : '' ?>>English</option>
+                <option value="sp" <?= $language === 'sp' ? ' selected' : '' ?>>Español</option>
             </select>
+            <button type="submit" class="btn btn-outline-secondary">Cambia</button>
         </form>
     </div>
     </div>
---
+++ includes/translations.php
@@ -0,0 +1,29 @@
+<?php
+
+$labels = [
+    'it' => [
+        'site_name' => 'ITA'
+    ],
+
+    'en' => [
+        'site_name' => 'ENG',
+    ],
+
+    'fr' => [
+        'site_name' => 'ESP'
+    ]
+];
+
+$host = 'localhost';
+$db   = 'ifoa_filesystem';
+$user = 'root';
+$pass = '';
+$dsn = "mysql:host=$host;dbname=$db";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+$pdo = new PDO($dsn, $user, $pass, $options);
