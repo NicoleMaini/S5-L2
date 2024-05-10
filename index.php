07e4cef45f15649192461f09326c9fe49de8b6c6
Nico_le <156796855+NicoleMaini@users.noreply.github.com>
2024-05-10 17:58:38 +0200

    fixed-2

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
 <?php
 require_once __DIR__ . '/includes/header.php';
 require_once __DIR__ . '/includes/nav.php';
-
-$host = 'localhost';
-$db   = 'ifoa_filesystem';
-$user = 'root';
-$pass = '';
-$dsn = "mysql:host=$host;dbname=$db";
-
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
-$pdo = new PDO($dsn, $user, $pass, $options);
+require_once __DIR__ . '/includes/db.php';
 
 $stmt = $pdo->prepare("SELECT title, content FROM news WHERE language = ?"); // ? placheholder sostitutivo per il valore, con il quale andremo a controllare il valore successivo
 $stmt->execute([$language]);
